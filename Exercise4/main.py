369f61f3df5021b02ac37772fa99e8d28aa23b25
Roman P <roman.percev@gmail.com>
2020-07-07 16:15:37 +0300

    Add CLI options
    Signed-off-by: Roman P <roman.percev@gmail.com>

--- Exercise4/main.py
+++ Exercise4/main.py
@@ -1,5 +1,7 @@
+import argparse
 import asyncio
 from datetime import datetime
+import os
 
 import aiofiles
 from aiofiles.threadpool.text import AsyncTextIOWrapper
@@ -50,7 +52,20 @@ async def start_chat_reader(host: str, port: int, log: str):
 
 
 if __name__ == '__main__':
+    current_dir = os.getcwd()
+    full_path = f'{current_dir}/chat.log'
+    parser = argparse.ArgumentParser(prog='Chat connector')
+    parser.formatter_class = argparse.ArgumentDefaultsHelpFormatter
+    parser.add_argument("--host", type=str, default='minechat.dvmn.org', help="Set server host")
+    parser.add_argument("--port", type=int, default=5000, help="Set server port")
+    parser.add_argument("--log", type=str, default=full_path, help="Path to log file")
+    args = parser.parse_args()
+
+    server_host = os.getenv("SERV_HOST", args.host)
+    server_port = int(os.getenv("SERV_PORT", args.port))
+    server_log = os.getenv("SERV_LOG", args.log)
+
     try:
-        asyncio.run(start_chat_reader('minechat.dvmn.org', 5000, 'chat.log'))
+        asyncio.run(start_chat_reader(server_host, server_port, server_log))
     except KeyboardInterrupt:
         print('Manually closed')
