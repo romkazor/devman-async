680cb209616ea2501b27cec443147cb7719219d4
romkazor <roman.percev@gmail.com>
2021-01-31 00:02:22 +0300

    Add CLI options.

--- Exercise4/chat_reader.py
+++ Exercise4/chat_reader.py
@@ -1,4 +1,5 @@
 import asyncio
+import configargparse
 import os
 
 import aiofiles
@@ -34,7 +35,17 @@ async def start_chat_reader(host: str, port: int, log: str):
 if __name__ == '__main__':
     history_file = os.path.abspath(__file__ + '/../minechat.history')
 
+    parser = configargparse.ArgParser()
+    parser.add_argument("--host", type=str, default='minechat.dvmn.org', help="Set server host", env_var='CHAT_HOST')
+    parser.add_argument("--port", type=int, default=5000, help="Set server port", env_var='CHAT_PORT')
+    parser.add_argument("--history", type=str, default=history_file, help='Path to log file', env_var='CHAT_HISTORY')
+    options = parser.parse_args()
+
+    chat_host = options.host
+    chat_port = options.port
+    chat_history = options.history
+
     try:
-        asyncio.run(start_chat_reader('minechat.dvmn.org', 5000, history_file))
+        asyncio.run(start_chat_reader(chat_host, chat_port, chat_history))
     except (KeyboardInterrupt, SystemExit):
         print('\nManually closed')
